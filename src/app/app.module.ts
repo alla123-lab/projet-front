03e210e31978a2788e2a92777227c0c3b250d104
alla ismail <wwwallaismail@gmail.com>
2022-03-31 15:51:12 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LeftListComponent } from './left-list/left-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LeftListComponent
   ],
   imports: [
     BrowserModule,
